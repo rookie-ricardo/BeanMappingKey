7efcbab15f06bf13584338a0aeec51160abcf51d
Rookie <Rookie.Ricardo@outlook.com>
2021-12-19 22:09:34 +0800

    add method way generate code

--- src/main/java/org/rookie/plugins/delegate/BeanMappingJavaDelegate.java
+++ src/main/java/org/rookie/plugins/delegate/BeanMappingJavaDelegate.java
@@ -2,24 +2,25 @@ package org.rookie.plugins.delegate;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiLocalVariable;
-import com.intellij.psi.PsiMethod;
+import com.intellij.psi.*;
+import com.intellij.psi.util.PsiTypesUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.rookie.plugins.utils.ClipboardUtil;
 import org.rookie.plugins.utils.IntellijNotifyUtil;
 
 import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-public class BeanMappingJavaDelegate implements BeanMappingDelegate{
-
-    private boolean isSuccessful = true;
+public class BeanMappingJavaDelegate implements BeanMappingDelegate {
 
     private static final String INFO_MSG = "BeanMapping : success !";
-
     private static final String ERROR_MSG = "BeanMapping : 当前所选对象不支持 (The currently selected object is not supported)";
+    private boolean isSuccessful = true;
 
     @Override
     public void exec(AnActionEvent e) {
@@ -49,7 +50,63 @@ public class BeanMappingJavaDelegate implements BeanMappingDelegate{
     }
 
     private void doPsiMethod(PsiMethod psiMethod) {
+        if (isNotAvailablePsiMethod(psiMethod)) {
+            return;
+        }
 
+        String fromVarName = psiMethod.getParameterList().getParameter(0).getName();
+        PsiClass from = PsiTypesUtil.getPsiClass(psiMethod.getParameterList().getParameter(0).getType());
+        PsiClass to = PsiTypesUtil.getPsiClass(psiMethod.getReturnType());
+
+        // process from field and getMethod
+        Map<String, String> fromFieldMap = Arrays.stream(from.getAllFields())
+                .collect(Collectors.toMap(PsiField::getName, PsiField::getName));
+
+        Arrays.stream(from.getAllMethods()).forEach(m -> {
+            if (m.getName().startsWith("get")) {
+                fromFieldMap.keySet().forEach(k -> {
+                    if (m.getName().equalsIgnoreCase(("get" + k))) {
+                        fromFieldMap.put(k, m.getName());
+                    }
+                });
+            }
+        });
+
+        StringBuilder context = new StringBuilder();
+
+        if (Arrays.stream(to.getInnerClasses()).anyMatch(c -> c.getName().contains("Builder"))) {
+            // builder model
+            context.append("\t\treturn ").append(to.getName()).append(".builder()\n");
+            Arrays.stream(to.getAllFields()).forEach(f -> {
+                String val = fromFieldMap.get(f.getName());
+                if (StringUtils.isNotEmpty(val)) {
+                    context.append("\t\t\t\t.").append(f.getName())
+                            .append("(").append(fromVarName).append(".").append(val).append("())\n");
+                }
+            });
+            context.append("\t\t\t\t.build();");
+
+        } else {
+            // set model
+            String localVar = String.valueOf(to.getName().charAt(0)).toLowerCase() + to.getName().substring(1);
+            context.append("\t\t").append(to.getName()).append(" ").append(localVar)
+                    .append(" = new ").append(to.getName()).append("();\n");
+            Arrays.stream(to.getAllFields()).forEach(f -> {
+                String val = fromFieldMap.get(f.getName());
+                if (StringUtils.isNotEmpty(val)) {
+                    context.append("\t\t").append(localVar).append(".").append("set")
+                            .append(String.valueOf(f.getName().charAt(0)).toUpperCase())
+                            .append(f.getName().substring(1))
+                            .append("(").append(fromVarName).append(".").append(val).append("());\n");
+                }
+            });
+            context.append("\t\treturn ").append(localVar).append(";");
+        }
+
+        Document document = FileDocumentManager.getInstance()
+                .getDocument(psiMethod.getContainingFile().getVirtualFile());
+        WriteCommandAction.runWriteCommandAction(psiMethod.getProject(),
+                () -> document.insertString(psiMethod.getBody().getTextOffset() + 1, "\n" + context));
     }
 
     private void doPsiClass(PsiClass psiClass, String localVar) {
@@ -82,7 +139,7 @@ public class BeanMappingJavaDelegate implements BeanMappingDelegate{
 
         StringBuilder context = new StringBuilder();
         if (StringUtils.isBlank(localVar)) {
-            localVar = psiClass.getName().charAt(0) + psiClass.getName().substring(1);
+            localVar = String.valueOf(psiClass.getName().charAt(0)).toLowerCase() + psiClass.getName().substring(1);
             context.append(psiClass.getName()).append(" ").append(localVar).append(" = new ")
                     .append(psiClass.getName()).append("();\n");
         }
@@ -109,6 +166,14 @@ public class BeanMappingJavaDelegate implements BeanMappingDelegate{
     private boolean isNotAvailablePsiClass(PsiClass psiClass) {
         if (psiClass.isInterface()) {
             doErrorNotify(psiClass.getProject());
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isNotAvailablePsiMethod(PsiMethod psiMethod) {
+        if (psiMethod.getParameterList().isEmpty() || PsiType.VOID.equals(psiMethod.getReturnType())) {
+            doErrorNotify(psiMethod.getProject());
             return true;
         }
         return false;
